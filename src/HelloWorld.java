3f9c7471e3b8f396de5ac5d71a9c3e0fcae58ddd
A.Pranathi <pa0682@srmist.edu.in>
2026-04-10 23:38:23 +0530

    UC2: Display Hello with command line argument

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello Pranathi");
-        }
+        System.out.println("Hello " + args[0]);
     }
 }
