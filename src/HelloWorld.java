ea1cbcee5d64bdd86a87590a8530bed96a9a7391
A.Pranathi <pa0682@srmist.edu.in>
2026-04-11 00:48:50 +0530

    UC6: Remove trailing space using substring

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,11 +2,16 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         if (args.length > 0) {
-            System.out.print("Hello ");
+            String result = "";
 
             for (String name : args) {
-                System.out.print(name + " ");
+                result += name + " ";
             }
+
+            // Remove last extra space
+            result = result.substring(0, result.length() - 1);
+
+            System.out.println("Hello " + result);
 
         } else {
             System.out.println("Hello, World!");
